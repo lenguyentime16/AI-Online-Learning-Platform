0a3016a28c664f4817ae7a2f99b470289f89a9a3
lenguyentime16 <lenguyentime16@gmail.com>
2025-08-08 08:39:14 +0700

    Project Setup + Authentication with using Clerk

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SignIn } from '@clerk/nextjs'
+
+export default function Page() {
+    return (
+        <div className='flex items-center justify-center h-screen'>
+            <SignIn />
+        </div>
+    )
+
+}
